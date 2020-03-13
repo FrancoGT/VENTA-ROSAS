e2f411400ebf9f31c9170c56aad545640c284808
FrancoGT <francogt8000@gmail.com>
2020-03-13 02:55:04 +0100

    Add files via upload

---
+++ sources/android/support/v7/recyclerview/R.java
@@ -0,0 +1,331 @@
+package android.support.v7.recyclerview;
+
+import com.android.tools.fd.runtime.IncrementalChange;
+import com.android.tools.fd.runtime.InstantReloadException;
+
+public final class R {
+    public static volatile transient /* synthetic */ IncrementalChange $change;
+
+    public static final class attr {
+        public static volatile transient /* synthetic */ IncrementalChange $change = null;
+        public static final int layoutManager = 2130772061;
+        public static final int reverseLayout = 2130772063;
+        public static final int spanCount = 2130772062;
+        public static final int stackFromEnd = 2130772064;
+
+        attr(Object[] objArr, InstantReloadException instantReloadException) {
+            String str = objArr[0];
+            switch (str.hashCode()) {
+                case -1968665286:
+                    return;
+                case 1602252263:
+                    this();
+                    return;
+                default:
+                    throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "android/support/v7/recyclerview/R$attr"}));
+            }
+        }
+
+        public static /* synthetic */ Object access$super(attr attr, String str, Object... objArr) {
+            switch (str.hashCode()) {
+                case -2128160755:
+                    return super.toString();
+                case -1600833221:
+                    super.wait(objArr[0].longValue(), objArr[1].intValue());
+                    return null;
+                case -1554832987:
+                    super.finalize();
+                    return null;
+                case -1166127280:
+                    super.notify();
+                    return null;
+                case -1021472056:
+                    super.wait(objArr[0].longValue());
+                    return null;
+                case -712101345:
+                    super.notifyAll();
+                    return null;
+                case 201261558:
+                    return super.getClass();
+                case 244142972:
+                    super.wait();
+                    return null;
+                case 1403628309:
+                    return new Integer(super.hashCode());
+                case 1814730534:
+                    return new Boolean(super.equals(objArr[0]));
+                case 2025021518:
+                    return super.clone();
+                default:
+                    throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "android/support/v7/recyclerview/R$attr"}));
+            }
+        }
+
+        public attr() {
+            IncrementalChange incrementalChange = $change;
+            if (incrementalChange != null) {
+                Object[] objArr = {objArr};
+                this((Object[]) incrementalChange.access$dispatch("init$args.([Ljava/lang/Object;)Ljava/lang/Object;", objArr), null);
+            }
+            if (incrementalChange != null) {
+                incrementalChange.access$dispatch("init$body.(Landroid/support/v7/recyclerview/R$attr;)V", this);
+            }
+        }
+    }
+
+    public static final class dimen {
+        public static volatile transient /* synthetic */ IncrementalChange $change = null;
+        public static final int item_touch_helper_max_drag_scroll_per_frame = 2131230823;
+
+        dimen(Object[] objArr, InstantReloadException instantReloadException) {
+            String str = objArr[0];
+            switch (str.hashCode()) {
+                case -1968665286:
+                    return;
+                case 1371250985:
+                    this();
+                    return;
+                default:
+                    throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "android/support/v7/recyclerview/R$dimen"}));
+            }
+        }
+
+        public static /* synthetic */ Object access$super(dimen dimen, String str, Object... objArr) {
+            switch (str.hashCode()) {
+                case -2128160755:
+                    return super.toString();
+                case -1600833221:
+                    super.wait(objArr[0].longValue(), objArr[1].intValue());
+                    return null;
+                case -1554832987:
+                    super.finalize();
+                    return null;
+                case -1166127280:
+                    super.notify();
+                    return null;
+                case -1021472056:
+                    super.wait(objArr[0].longValue());
+                    return null;
+                case -712101345:
+                    super.notifyAll();
+                    return null;
+                case 201261558:
+                    return super.getClass();
+                case 244142972:
+                    super.wait();
+                    return null;
+                case 1403628309:
+                    return new Integer(super.hashCode());
+                case 1814730534:
+                    return new Boolean(super.equals(objArr[0]));
+                case 2025021518:
+                    return super.clone();
+                default:
+                    throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "android/support/v7/recyclerview/R$dimen"}));
+            }
+        }
+
+        public dimen() {
+            IncrementalChange incrementalChange = $change;
+            if (incrementalChange != null) {
+                Object[] objArr = {objArr};
+                this((Object[]) incrementalChange.access$dispatch("init$args.([Ljava/lang/Object;)Ljava/lang/Object;", objArr), null);
+            }
+            if (incrementalChange != null) {
+                incrementalChange.access$dispatch("init$body.(Landroid/support/v7/recyclerview/R$dimen;)V", this);
+            }
+        }
+    }
+
+    public static final class id {
+        public static volatile transient /* synthetic */ IncrementalChange $change = null;
+        public static final int item_touch_helper_previous_elevation = 2131492869;
+
+        id(Object[] objArr, InstantReloadException instantReloadException) {
+            String str = objArr[0];
+            switch (str.hashCode()) {
+                case -1971704879:
+                    this();
+                    return;
+                case -1968665286:
+                    return;
+                default:
+                    throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "android/support/v7/recyclerview/R$id"}));
+            }
+        }
+
+        public static /* synthetic */ Object access$super(id idVar, String str, Object... objArr) {
+            switch (str.hashCode()) {
+                case -2128160755:
+                    return super.toString();
+                case -1600833221:
+                    super.wait(objArr[0].longValue(), objArr[1].intValue());
+                    return null;
+                case -1554832987:
+                    super.finalize();
+                    return null;
+                case -1166127280:
+                    super.notify();
+                    return null;
+                case -1021472056:
+                    super.wait(objArr[0].longValue());
+                    return null;
+                case -712101345:
+                    super.notifyAll();
+                    return null;
+                case 201261558:
+                    return super.getClass();
+                case 244142972:
+                    super.wait();
+                    return null;
+                case 1403628309:
+                    return new Integer(super.hashCode());
+                case 1814730534:
+                    return new Boolean(super.equals(objArr[0]));
+                case 2025021518:
+                    return super.clone();
+                default:
+                    throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "android/support/v7/recyclerview/R$id"}));
+            }
+        }
+
+        public id() {
+            IncrementalChange incrementalChange = $change;
+            if (incrementalChange != null) {
+                Object[] objArr = {objArr};
+                this((Object[]) incrementalChange.access$dispatch("init$args.([Ljava/lang/Object;)Ljava/lang/Object;", objArr), null);
+            }
+            if (incrementalChange != null) {
+                incrementalChange.access$dispatch("init$body.(Landroid/support/v7/recyclerview/R$id;)V", this);
+            }
+        }
+    }
+
+    public static final class styleable {
+        public static volatile transient /* synthetic */ IncrementalChange $change = null;
+        public static final int[] RecyclerView = {16842948, com.example.alvaro.apptercero.R.attr.layoutManager, com.example.alvaro.apptercero.R.attr.spanCount, com.example.alvaro.apptercero.R.attr.reverseLayout, com.example.alvaro.apptercero.R.attr.stackFromEnd};
+        public static final int RecyclerView_android_orientation = 0;
+        public static final int RecyclerView_layoutManager = 1;
+        public static final int RecyclerView_reverseLayout = 3;
+        public static final int RecyclerView_spanCount = 2;
+        public static final int RecyclerView_stackFromEnd = 4;
+
+        styleable(Object[] objArr, InstantReloadException instantReloadException) {
+            String str = objArr[0];
+            switch (str.hashCode()) {
+                case -1968665286:
+                    return;
+                case -1121327357:
+                    this();
+                    return;
+                default:
+                    throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "android/support/v7/recyclerview/R$styleable"}));
+            }
+        }
+
+        public static /* synthetic */ Object access$super(styleable styleable, String str, Object... objArr) {
+            switch (str.hashCode()) {
+                case -2128160755:
+                    return super.toString();
+                case -1600833221:
+                    super.wait(objArr[0].longValue(), objArr[1].intValue());
+                    return null;
+                case -1554832987:
+                    super.finalize();
+                    return null;
+                case -1166127280:
+                    super.notify();
+                    return null;
+                case -1021472056:
+                    super.wait(objArr[0].longValue());
+                    return null;
+                case -712101345:
+                    super.notifyAll();
+                    return null;
+                case 201261558:
+                    return super.getClass();
+                case 244142972:
+                    super.wait();
+                    return null;
+                case 1403628309:
+                    return new Integer(super.hashCode());
+                case 1814730534:
+                    return new Boolean(super.equals(objArr[0]));
+                case 2025021518:
+                    return super.clone();
+                default:
+                    throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "android/support/v7/recyclerview/R$styleable"}));
+            }
+        }
+
+        public styleable() {
+            IncrementalChange incrementalChange = $change;
+            if (incrementalChange != null) {
+                Object[] objArr = {objArr};
+                this((Object[]) incrementalChange.access$dispatch("init$args.([Ljava/lang/Object;)Ljava/lang/Object;", objArr), null);
+            }
+            if (incrementalChange != null) {
+                incrementalChange.access$dispatch("init$body.(Landroid/support/v7/recyclerview/R$styleable;)V", this);
+            }
+        }
+    }
+
+    R(Object[] objArr, InstantReloadException instantReloadException) {
+        String str = objArr[0];
+        switch (str.hashCode()) {
+            case -1968665286:
+                return;
+            case -497074724:
+                this();
+                return;
+            default:
+                throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "android/support/v7/recyclerview/R"}));
+        }
+    }
+
+    public static /* synthetic */ Object access$super(R r, String str, Object... objArr) {
+        switch (str.hashCode()) {
+            case -2128160755:
+                return super.toString();
+            case -1600833221:
+                super.wait(objArr[0].longValue(), objArr[1].intValue());
+                return null;
+            case -1554832987:
+                super.finalize();
+                return null;
+            case -1166127280:
+                super.notify();
+                return null;
+            case -1021472056:
+                super.wait(objArr[0].longValue());
+                return null;
+            case -712101345:
+                super.notifyAll();
+                return null;
+            case 201261558:
+                return super.getClass();
+            case 244142972:
+                super.wait();
+                return null;
+            case 1403628309:
+                return new Integer(super.hashCode());
+            case 1814730534:
+                return new Boolean(super.equals(objArr[0]));
+            case 2025021518:
+                return super.clone();
+            default:
+                throw new InstantReloadException(String.format("String switch could not find '%s' with hashcode %s in %s", new Object[]{str, Integer.valueOf(str.hashCode()), "android/support/v7/recyclerview/R"}));
+        }
+    }
+
+    public R() {
+        IncrementalChange incrementalChange = $change;
+        if (incrementalChange != null) {
+            Object[] objArr = {objArr};
+            this((Object[]) incrementalChange.access$dispatch("init$args.([Ljava/lang/Object;)Ljava/lang/Object;", objArr), null);
+        }
+        if (incrementalChange != null) {
+            incrementalChange.access$dispatch("init$body.(Landroid/support/v7/recyclerview/R;)V", this);
+        }
+    }
+}
